3a42868287f7b00a92326914e9980d49f3d5e1ea
Gabriel <gabriel.sgaspar@yahoo.com.br>
2020-03-27 15:16:36 -0300

    implementa efeito ao pressionar as teclas do xylophone

--- app/src/main/java/com/example/xylophone/MainActivity.java
+++ app/src/main/java/com/example/xylophone/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.xylophone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
     Button buttonG;
     Button buttonA;
     Button buttonB;
-
-    MediaPlayer player = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void registerForEvents(Button[] buttons) {
+
         for(final Button button : buttons){
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -57,10 +58,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void buttonPressed(Button button) {
-        playSong(button.getText());
+
+        float alpha = (float) 0.5;
+        button.setAlpha(alpha);
+
+        new ButtonAlphaEffect(button).execute();
+
+        String musicalNote = button.getText().toString();
+        playSong(musicalNote);
+
     }
 
-    private void playSong(CharSequence musicalNote) {
-        //player.setDataSource();
+    private void playSong(String musicalNote) {
+        String filename = "android.resource://" + this.getPackageName() + "/raw/" + musicalNote.toLowerCase();
+        MediaPlayer player = new MediaPlayer();
+
+        registerForPlayerEvent(player);
+
+        try { player.setDataSource(this, Uri.parse(filename)); } catch (Exception e) {}
+        try { player.prepare(); } catch (Exception e) {}
+        player.start();
     }
+
+    private void registerForPlayerEvent(MediaPlayer player) {
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
+    }
+
+    private class ButtonAlphaEffect extends AsyncTask<Void, Void, Void> {
+
+        Button button;
+
+        ButtonAlphaEffect(Button button){
+            this.button = button;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            this.button.setAlpha(1);
+        }
+    }
+
 }
